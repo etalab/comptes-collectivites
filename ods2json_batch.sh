42db5a9c97f8aaf4315726810f0ce83bf88b11cd
cquest <cquest@openstreetmap.fr>
2018-03-18 16:26:53 +0100

    suppression ancien CSV avant traitement

--- ods2json_batch.sh
+++ ods2json_batch.sh
@@ -1,7 +1,8 @@
 # conversion d'une série de fichiers annuels
 # exemple: ./ods2json_batch.sh 2016
 
-for D in $(ls -1 $1)
+rm -f $1/*.csv
+for D in $(ls -1 $1/ )
 do
   echo $D
   ./ods2json.py CSV $D $1/$D/*.ods > $1/$D.csv
