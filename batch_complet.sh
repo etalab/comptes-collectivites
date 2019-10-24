c59b9631e24ba7833b6f1fa8ee91a5a9c34118f0
cquest <cquest@openstreetmap.fr>
2019-10-24 15:09:30 +0200

    mise à jour 2018

--- batch_complet.sh
+++ batch_complet.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-DEBUT=2017
-FIN=2017
+DEBUT=2000
+FIN=2018
 
 echo "Téléchargement des fichiers ODS"
 for DEP in `seq -w 01 19` 2A 2B `seq 21 95` `seq 971 974` 976
 do
   # création des dossiers pour ranger les fichiers
   for ANNEE in `seq $DEBUT $FIN`; do mkdir -p $ANNEE/$DEP; done
-  ./download.py $DEP $DEBUT $FIN
+  ./download.py $DEP $FIN $FIN
 done
 
 echo "Conversion parallelisée des ODS en CSV"
 seq $DEBUT $FIN | parallel ./ods2json_batch.sh :::
 
 echo "Cumul fichier $DEBUT-$FIN"
-head -n 1 $ANNEE/$DEBUT.csv > comptes_communes_$DEBUT-$FIN.csv
+head -n 1 $DEBUT/01.csv > comptes_communes_$DEBUT-$FIN.csv
 for ANNEE in `seq $DEBUT $FIN`
 do
   head -n 1 $ANNEE/01.csv > $ANNEE.csv
   for F in $ANNEE/*.csv
   do
-    tail -n +2 $F >> $ANNEE.csv
-    tail -n +2 $F >> comptes_communes_$DEBUT-$FIN.csv
+    tail -n +2 $F | grep -v '^$' >> $ANNEE.csv
+    tail -n +2 $F | grep -v '^$' >> comptes_communes_$DEBUT-$FIN.csv
   done
 done
